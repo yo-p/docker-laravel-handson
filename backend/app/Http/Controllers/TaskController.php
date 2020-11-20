c6d7a5e0211ffac8121a21b0e2659ef827310a5b
小林　良輝 <papapapipipi42@gmail.com>
2020-11-20 17:16:18 +0900

    Add TaskAttributes

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Folder;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index()
+    public function index(int $id)
     {
         $folders = Folder::all();
 
+        $current_folder = Folder::find($id);
+
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
+
         return view('tasks/index', [
             'folders' => $folders,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
         ]);
     }
 }
---
+++ backend/app/Models/Task.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    const STATUS = [
+        1 => [ 'label' => '未着手', 'class' => 'label-danger' ],
+        2 => [ 'label' => '着手中', 'class' => 'label-info' ],
+        3 => [ 'label' => '完了', 'class' => ''],
+    ];
+
+    public function getStatusLabelAttribute()
+    {
+        $status = $this->attributes['status'];
+
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        return self::STATUS[$status]['label'];
+    }
+
+    public function getStatusClassAttribute()
+    {
+        $status = $this->attributes['status'];
+
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        return self::STATUS[$status]['class'];
+    }
+
+    public function getFormattedDueDateAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d', $this->attributes['due_date'])->format('Y/m/d');
+    }
+}
