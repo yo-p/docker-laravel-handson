b438cfd1fc87823ffb62aa97a0dccac3d2a4abe9
小林　良輝 <papapapipipi42@gmail.com>
2020-11-24 17:29:28 +0900

    AddTaskCreateForm

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -25,4 +25,11 @@ class TaskController extends Controller
             'tasks' => $tasks,
         ]);
     }
+
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id
+        ]);
+    }
 }
